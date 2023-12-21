19453da0e7c9283405f5b1b2b6bde60d7548e4c4
Bart Lewicz <117043387+gitbarlew@users.noreply.github.com>
2023-12-21 15:52:50 +0100

    Update main.py
    Improved code reuse and formatting.

--- main.py
+++ main.py
@@ -7,152 +7,87 @@ import time
 import openpyxl
 
 nltk.download('vader_lexicon')
-client = OpenAI(api_key='put_your_openAI_API_Key_here',
-                organization='configure_organization_if_applicable', )
+client = OpenAI(api_key='OPENAI_API_KEY_GOES_HERE',
+                organization='CONFIGURE_ORGANIZATION_IF_NEEDED', )
+
+
+# Load data from Excel - specify filename & sheet name to match your input document
+df = pd.read_excel('input.xlsx', sheet_name='Sheet1')
+
+# Specify the column name containing text data for analysis
+column_name = 'TextColumn'
 
 
 def analyze_sentiment_nltk(text):
+    """
+    Analyze sentiment using NLTK's Sentiment Intensity Analyzer & Vader Lexicon.
+    """
     sia = SentimentIntensityAnalyzer()
     score = sia.polarity_scores(text)['compound']
     return 'positive' if score > 0.1 else 'negative' if score < -0.1 else 'neutral'
 
 
 def analyze_sentiment_textblob(text):
+    """
+    Analyze sentiment using TextBlob.
+    """
     score = TextBlob(text).sentiment.polarity
     return 'positive' if score > 0.1 else 'negative' if score < -0.1 else 'neutral'
 
 
-def analyze_sentiment_openai3(text):
-
+def analyze_sentiment_openai(model, text, temperature=1.0, max_tokens=100, top_p=1.0):
+    """
+    Analyze sentiment using OpenAI's GPT models.
+    """
     response = client.chat.completions.create(
-        model='gpt-3.5-turbo',
+        model=model,
         messages=[
             {
                 'role': 'system',
-                'content': 'You will be provided with a text, and your task is '
-                           'to classify its sentiment as positive, neutral, or negative. '
-                           'Data might not have been cleand up, '
-                           ' and can contain empty responses or non-alphanumeric characters. If you are not able'
-                           ' to ascertain the sentiment return Unknown.'
-                           ' Respond only with one of the values: '
-                           ' Postive, Negative, Neutral or Unknown.'
+                'content': ('You will be provided with a text, and your task is '
+                            'to classify its sentiment as positive, neutral, or negative. '
+                            'Data might not have been cleaned up, '
+                            'and can contain empty responses or non-alphanumeric characters. '
+                            'If you are not able to ascertain the sentiment, return Unknown. '
+                            'Respond only with one of the values: '
+                            'Positive, Negative, Neutral, or Unknown.')
             },
             {
                 'role': 'user',
-                'content': str(text)}
+                'content': str(text)
+            }
         ],
+        temperature=temperature,
+        max_tokens=max_tokens,
+        top_p=top_p
     )
-    sentiment = response.choices[0].message.content
-    return sentiment
+    return response.choices[0].message.content
 
 
-def analyze_sentiment_openai4(text):
-
-    response = client.chat.completions.create(
-        model='gpt-4',
-        messages=[
-            {
-                'role': 'system',
-                'content': 'You will be provided with a text, and your task is '
-                           'to classify its sentiment as positive, neutral, or negative. '
-                           'Data might not have been cleand up, '
-                           ' and can contain empty responses or non-alphanumeric characters. If you are not able'
-                           ' to ascertain the sentiment return Unknown.'
-                           ' Respond only with one of the values: '
-                           ' Postive, Negative, Neutral or Unknown.'
-            },
-            {
-                'role': 'user',
-                'content': str(text)}
-        ],
-    )
-    sentiment = response.choices[0].message.content
-    return sentiment
-
-
-def analyze_sentiment_openai4finetuned(text):
-
-    response = client.chat.completions.create(
-        model='gpt-4',
-        messages=[
-            {
-                'role': 'system',
-                'content': 'You will be provided with a text, and your task is '
-                           'to classify its sentiment as positive, neutral, or negative. '
-                           'Data might not have been cleand up, '
-                           ' and can contain empty responses or non-alphanumeric characters. If you are not able'
-                           ' to ascertain the sentiment return Unknown.'
-                           ' Respond only with one of the values: '
-                           ' Postive, Negative, Neutral or Unknown.'
-            },
-            {
-                'role': 'user',
-                'content': str(text)}
-        ],
-        temperature=0.3,
-        max_tokens=32,
-        top_p=0.8
-    )
-    sentiment = response.choices[0].message.content
-    return sentiment
-
-
-def analyze_sentiment_openai3finetuned(text):
-
-    response = client.chat.completions.create(
-        model='gpt-3.5-turbo',
-        messages=[
-            {
-                'role': 'system',
-                'content': 'You will be provided with a text, and your task is '
-                           'to classify its sentiment as positive, neutral, or negative. '
-                           'Data might not have been cleand up, '
-                           ' and can contain empty responses or non-alphanumeric characters. If you are not able'
-                           ' to ascertain the sentiment return Unknown.'
-                           ' Respond only with one of the values: '
-                           ' Postive, Negative, Neutral or Unknown.'
-            },
-            {
-                'role': 'user',
-                'content': str(text)}
-        ],
-        temperature=0.3,
-        max_tokens=32,
-        top_p=0.8
-    )
-    sentiment = response.choices[0].message.content
-    return sentiment
-
-
-# Load data from Excel - change filename & sheet name to match your input document
-df = pd.read_excel('input.xlsx', sheet_name='Sheet1')
-# Provide name of the column containing data to analyze.
-column_name = 'Can you explain your answer?'
-
-start_time1 = time.time()
+# Apply sentiment analysis functions to the specified column and measure execution time
+start_time = time.time()
 df['NLTK_Sentiment'] = df[column_name].astype(str).apply(analyze_sentiment_nltk)
-print("--- %s seconds ---" % (time.time() - start_time1))
+print(f"NLTK Analysis Time: {time.time() - start_time} seconds")
 
-start_time2 = time.time()
+start_time = time.time()
 df['TextBlob_Sentiment'] = df[column_name].astype(str).apply(analyze_sentiment_textblob)
-print("--- %s seconds ---" % (time.time() - start_time2))
+print(f"TextBlob Analysis Time: {time.time() - start_time} seconds")
 
-start_time3 = time.time()
-df['ChatGPT3.5'] = df[column_name].astype(str).apply(analyze_sentiment_openai3)
-print("--- %s seconds ---" % (time.time() - start_time3))
+start_time = time.time()
+df['ChatGPT3.5'] = df[column_name].astype(str).apply(lambda x: analyze_sentiment_openai('gpt-3.5-turbo', x))
+print(f"ChatGPT 3.5 Analysis Time: {time.time() - start_time} seconds")
 
-start_time4 = time.time()
-df['ChatGPT3.5-finetuned'] = df[column_name].astype(str).apply(analyze_sentiment_openai3finetuned)
-print("--- %s seconds ---" % (time.time() - start_time4))
+start_time = time.time()
+df['ChatGPT3.5-finetuned'] = df[column_name].astype(str).apply(lambda x: analyze_sentiment_openai('gpt-3.5-turbo', x, temperature=0.3, max_tokens=32, top_p=0.8))
+print(f"ChatGPT 3.5 Finetuned Analysis Time: {time.time() - start_time} seconds")
 
-start_time5 = time.time()
-df['ChatGPT4.5'] = df[column_name].astype(str).apply(analyze_sentiment_openai4)
-print("--- %s seconds ---" % (time.time() - start_time5))
+start_time = time.time()
+df['ChatGPT4'] = df[column_name].astype(str).apply(lambda x: analyze_sentiment_openai('gpt-4', x))
+print(f"ChatGPT 4 Analysis Time: {time.time() - start_time} seconds")
 
-start_time6 = time.time()
-df['ChatGPT4.5-finetuned'] = df[column_name].astype(str).apply(analyze_sentiment_openai4finetuned)
-print("--- %s seconds ---" % (time.time() - start_time6))
-
+start_time = time.time()
+df['ChatGPT4-finetuned'] = df[column_name].astype(str).apply(lambda x: analyze_sentiment_openai('gpt-4', x, temperature=0.3, max_tokens=32, top_p=0.8))
+print(f"ChatGPT 4 Finetuned Analysis Time: {time.time() - start_time} seconds")
 
 # Write results to a new Excel file
-df.to_excel('benchmark_results.xlsx', index=False)
+df.to_excel('benchmark_results_new.xlsx', index=False)
